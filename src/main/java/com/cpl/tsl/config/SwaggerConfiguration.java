56766e642bae5c55d59203029494dd7ef927003b
lililiang <lililiang@cestc.cn>
2022-06-15 10:42:06 +0800

    <feat><tsl>升级原始swagger文档为Swagger-Bootstrap-UI

--- src/main/java/com/cpl/tsl/config/SwaggerConfiguration.java
+++ src/main/java/com/cpl/tsl/config/SwaggerConfiguration.java
@@ -1,37 +1,76 @@
 package com.cpl.tsl.config;
 
+import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * 基于Swagger生成API文档
- *
- * @EnableOpenApi：启动OpenApi的类
- * @author: lll
- */
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @Configuration
-@EnableOpenApi
-public class SwaggerConfiguration {
+@EnableSwagger2
+@EnableSwaggerBootstrapUI
+public class SwaggerConfiguration implements WebMvcConfigurer {
 
+    /**
+     * 显示swagger-ui.html文档展示页，还必须注入swagger资源：
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    /**
+     * 帮助中心 (不同的模块这里分不同的包扫描basePackage)
+     * Docket 可以配置多个
+     *
+     * @return
+     */
     @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.OAS_30).apiInfo(apiInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com.cpl.tsl.controller")).paths(PathSelectors.any())
-                .build();
+    public Docket assist() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(setRequestHeaders())
+                .apiInfo(new ApiInfo("tsl接口文档", "tsl接口文档", "V1.0", "https://github.com/CodingPandaLLL/tsl", "CodingPandaLLL", "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0"))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.cpl.tsl.controller"))
+                //加了ApiOperation注解的类，才生成接口文档
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build()
+                .groupName("TSL");
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("Tsl API文档").description("Tsl API文档")
-//                .contact(new Contact("联系人", "www.baidu.com", "286279186@qq.com"))
-                .version("1.0").build();
+    /**
+     * 设置请求头
+     *
+     * @return
+     */
+    private List<Parameter> setRequestHeaders() {
+        ParameterBuilder ticketPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<Parameter>();
+        ticketPar.name("token").description("用户token")
+                .modelRef(new ModelRef("string")).parameterType("header")
+                .required(false).build(); //header中的ticket参数非必填，传空也可以
+        pars.add(ticketPar.build());      //根据每个方法名也知道当前方法在设置什么参数
+        return pars;
     }
-
 }
 
